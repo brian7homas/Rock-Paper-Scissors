e225e79a4d20ea928490dc3ba6a3b095066976b0
Brian Thomas <btdeveloper125@gmail.com>
2023-06-09 15:37:46 -0600

    refactor(Button): BUTTON DATA
    iterate through data array to create each button

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { css } from '@emotion/react'
 import Button from './Button'
 import Pentagon from '../svg/bg-pentagon.svg'
 import Data from '../data/data'
@@ -19,8 +20,20 @@ const Board = () => {
             place-self: center;
             `}
       />
+      {
+        Data.map((item) => {
+          return(
+            <Button
+              color={item.color}
+              shadow={item.shadow}
+              icon={item.icon}
+              position={item.position}
+            />
+          )
+        })
+      }
+      
     </BoardContainer>
-  )
+  )  
 }
-
 export default Board
