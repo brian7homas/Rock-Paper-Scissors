d391085b2e249b7823e59223ab29d7f66de7eb34
Brian Thomas <btdeveloper125@gmail.com>
2023-06-09 15:33:22 -0600

    refactor(Board): PENTAGON STYLES
    styled boardgame background

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import styled from "@emotion/styled";
 
 const BoardContainer = styled.section`
-  margin: 0 auto;
+  margin: 5em auto 0 auto;
+  position:relative;
+  display: grid;
 `
 const Board = () => {
   return(
     <BoardContainer>
-      Board
+      <Pentagon 
+        css={css`
+            position:absolute;
+            z-index:0;
+            place-self: center;
+            `}
+      />
     </BoardContainer>
   )
 }
