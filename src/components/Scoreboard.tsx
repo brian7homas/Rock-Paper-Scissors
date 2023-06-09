54d3e253513ead475c64ae3459d2ccb32b380f14
Brian Thomas <btdeveloper125@gmail.com>
2023-06-09 11:22:47 -0600

    refactor(Scoreboard): CONTAINER
    setup container to calculate side margins

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import styled from "@emotion/styled";
 
 const ScoreboardContainer = styled.section`
-  margin: 0 auto;
+  margin: 5em calc((100% - 30em)/3);
+  padding: 1.5em 3em;
+  border: 2px solid white;
+  border-radius: 20px;
+  display:flex;
+  align-items: center;
+  justify-content: space-between;
+  line-height:2em;
+  letter-spacing: .02em;
+`
 `
 const Scoreboard = () => {
   return(
