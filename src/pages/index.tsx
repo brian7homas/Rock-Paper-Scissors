2683212316b1508e252c445daa4d4e924b37a1e7
Brian Thomas <btdeveloper125@gmail.com>
2023-06-10 15:34:31 -0600

    refactor(Index): SHORT SCREENS
    allow items to not overflow when screen is less than 600px

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ const MainContainer = styled.main`
   background: radial-gradient(circle, rgba(31,55,86,1) 21%, rgba(29,47,80,1) 45%, rgba(20,21,57,1) 92%);
   height:100vh;
   justify-content:space-between;
+  overflow-y: scroll;
 `
 const IndexPage: React.FC<PageProps> = () => {
   const [points, addPoints] = useState(0)
