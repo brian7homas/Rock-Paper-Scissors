0b242e44c9b52c05ce7928f2ed6d086dc81e7ac3
Brian Thomas <btdeveloper125@gmail.com>
2023-06-11 12:31:11 -0600

    refactor(StartRound) ANIMATION
    animate labels then set onChange object player value with what player has picked

--- src/animations/StartRoung.tsx
+++ src/animations/StartRoung.tsx
@@ -10,6 +10,9 @@ const StartRound = (name:any, animateOut:any, color:any) => {
     .fromTo(`.btn-container--${name}`, {background:'transparent'}, {ease:"slow(0.7, 0.7, false)", background:`${color}`}, '<=1.25')
     .to('.opponent-inlay', { display: 'flex', position: 'relative', visibility: 'visible', opacity: 1}, '<')
     .to('.opponent-container', { transform: 'scale(1.5)', display: 'flex', position: 'relative', visibility: 'visible', opacity: 1}, '<')
+    .to(['.player-label','.house-label'], { display: 'block', onComplete:() => {
+      onChange.player = name
+    }})
   return tl.play()
 }
 
