f51e23fbf4f7e083902403a1e3dd965bf7f8c245
Brian Thomas <btdeveloper125@gmail.com>
2023-06-11 12:31:23 -0600

    refactor(StartRound) ANIMATION
    marker

--- src/animations/StartRoung.tsx
+++ src/animations/StartRoung.tsx
@@ -1,10 +1,11 @@
 import gsap from 'gsap'
 
 let tl = gsap.timeline({paused: true})
-const StartRound = (name:any, animateOut:any, color:any) => {
+const StartRound = (name:any, animateOut:any, color:any, onChange:any) => {
   tl
     .to(`.btn-container--${name}`, { transform: 'scale(1.5)', zIndex:4 })
     .to([animateOut, '.pentagon'], { stagger: .2, duration: .15, opacity:0, display:'none' }, '<')
+    // Delay
     .to(`.board-container`, 2,{ ease: 'custom',  width: '50vw', display:'flex', justifyContent: 'space-between' }, '+=.5')
     .to(`.btn-container--${name}`, { position: 'initial'}, '<')
     .fromTo(`.btn-container--${name}`, {background:'transparent'}, {ease:"slow(0.7, 0.7, false)", background:`${color}`}, '<=1.25')
