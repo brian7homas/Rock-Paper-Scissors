fc97c4c9995e934ad4f676521df3032e69f45c4d
Brian Thomas <btdeveloper125@gmail.com>
2023-06-11 14:39:48 -0600

    refactor(Board): MOVED LOGIC
    organized import code
    moved start round animation and rules to board - easier to pass down round data to restart component
    created seperate rules file to handle who wins

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,16 +2,55 @@ import React from "react";
 import { lazy, useState } from "react";
 import styled from "@emotion/styled";
 import { css } from '@emotion/react'
+// COMPONENETS
 import Button from './Button'
+import Restart from "./Restart";
+// SVG
 import Pentagon from '../svg/bg-pentagon.svg'
+// DATA
 import Data from '../data/data'
+import Rules from '../data/rules'
+// ANIMATION
+import StartRound from "../animations/StartRoung";
+// LAZY
 const Opponent = lazy(() => import('../components/Opponent'))
 
 const Board = (props) => {
   const [load, setLoad] = useState(false);
+  let changeName = {
+    player:'',
+    house:''
+  }
+  let icon: React.JSX.Element, bg: string, name:any
+  const OpponentIcon = () => {
+    const randIndex = Math.floor(Math.random() * Data.length)
+    name = Data[randIndex].name
+    icon = Data[randIndex].icon
+    bg = Data[randIndex].color
+  }
+  const startRound = async (name:string) => {
+    let buttons = gsap.utils.toArray(".btn")
+    let animateOut:any = []
+    
+    // find matching class/element
+    const findMatches = (arr:any, val:any) => arr.filter((item:any, i:number) => {
+      
+      // find elements to animate out
+      if(item.classList[1] != `btn-container--${val}`){
+        animateOut.push(item)
+      }
+      
+      // matching class name sits in 2nd position of classList array/object
+      return item.classList[1] === `btn-container--${val}`
+    }).length;
+    findMatches(buttons, name)
+    StartRound(name, animateOut, props.color, changeName).then(() => {
+      Rules(changeName.player, props.housePick)
+    })
+  }
+  // STYLES
   const breakpoints = [685]
   const mqHeight = breakpoints.map(bp => `@media screen and (min-height: ${bp}px)`)
-  let icon: React.JSX.Element, bg: string, name:any
   const BoardContainer = styled.section`
   margin: 8em auto 0 auto;
   position:relative;
@@ -42,12 +81,7 @@ const Board = (props) => {
   right: -1em;
   font-size: 2em;
   `
-  const OpponentIcon = () => {
-    const randIndex = Math.floor(Math.random() * Data.length)
-    name = Data[randIndex].name
-    icon = Data[randIndex].icon
-    bg = Data[randIndex].color
-  }
+  
   OpponentIcon()
   return(
     <BoardContainer className="board-container">
@@ -77,6 +111,7 @@ const Board = (props) => {
               houseBg={bg}
               houseIcon={icon}
               housePick={name}
+              startRound={startRound}
             />
           )
         })
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -34,77 +34,13 @@ const Button = (props:any) => {
     align-items:center;
     cursor:pointer;
   `
-  let changeName = {
-    player:'',
-    house:''
-  }
-
-  const startRound = async (name:string) => {
-    let buttons = gsap.utils.toArray(".btn")
-    let animateOut:any = []
-    
-    // find matching class/element
-    const findMatches = (arr:any, val:any) => arr.filter((item:any, i:number) => {
-      
-      // find elements to animate out
-      if(item.classList[1] != `btn-container--${val}`){
-        animateOut.push(item)
-      }
-      
-      // matching class name sits in 2nd position of classList array/object
-      return item.classList[1] === `btn-container--${val}`
-    }).length;
-    findMatches(buttons, name)
-    StartRound(name, animateOut, props.color, changeName).then(() => {
-      Rules(changeName.player, props.housePick)
-    })
-  }
-  const Rules = (player:string, house:string) => {
-    switch(player){
-      case 'scissors':
-        if(house == 'spock') console.log('house: spock - HOUSE WINS')
-        if(house == 'lizard') console.log('house: lizard - YOU WIN')
-        if(house == 'rock') console.log('house: rock - HOUSE WINS')
-        if(house == 'paper') console.log('house: paper - YOU WIN')
-        if(house == 'scissors') console.log('house: scissors - DRAW')
-        break
-      case 'paper':
-        if(house == 'spock') console.log('house: spock - YOU WIN')
-        if(house == 'lizard') console.log('house: lizard - HOUSE WINS')
-        if(house == 'rock') console.log('house: rock - YOU WIN')
-        if(house == 'paper') console.log('house: paper - DRAW')
-        if(house == 'scissors') console.log('house: scissors - HOUSE WINS')
-        break
-      case 'rock':
-        if(house == 'spock') console.log('house: spock - HOUSE WINS')
-        if(house == 'lizard') console.log('house: lizard - YOU WIN')
-        if(house == 'rock') console.log('house: rock - DRAW')
-        if(house == 'paper') console.log('house: paper - HOUSE WINS')
-        if(house == 'scissors') console.log('house: scissors - YOU WIN')
-        break
-      case 'lizard':
-        if(house == 'spock') console.log('house: spock - YOU WIN')
-        if(house == 'lizard') console.log('house: lizard - DRAW')
-        if(house == 'rock') console.log('house: rock - HOUSE WINS')
-        if(house == 'paper') console.log('house: paper - YOU WIN')
-        if(house == 'scissors') console.log('house: scissors - HOUSE WINS')
-        break
-      case 'spock':
-        if(house == 'spock') console.log('house: spock - DRAW')
-        if(house == 'lizard') console.log('house: lizard - HOUSE WINS')
-        if(house == 'rock') console.log('house: rock - YOU WIN')
-        if(house == 'paper') console.log('house: paper - HOUSE WINS')
-        if(house == 'scissors') console.log('house: scissors - YOU WIN')
-        break
-    }
-  }
   return(
     <ButtonContainer className={`btn btn-container--${props.name}`}>
       <ButtonInlay 
         className={`btn-inlay--${props.name}`}
         onClick={async () => {
           props.opponent
-          return await startRound(props.name); 
+          return await props.startRound(props.name); 
           }}
         >
         {props.icon}
