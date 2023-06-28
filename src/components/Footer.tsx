32d285cca9c4aba1844e6912ccda28cf9e94c03b
Brian Thomas <btdeveloper125@gmail.com>
2023-06-28 10:30:01 -0600

    refactor(Board): STYLE UPDATE TO USE GSAP FLIP
    updates to help match design files
    various resets to positions, margins and font sizes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,22 +3,29 @@ import styled from "@emotion/styled";
 import { css } from '@emotion/react'
 import RulesAnimation from "../animations/RulesOverlay";
 import Rules from '../svg/image-rules-bonus.svg'
-const breakpoints = [685, 475]
+const breakpoints = [685, 475, 414, 375, 320]
 
 const mqHeight = breakpoints.map(bp => `@media screen and (max-height: ${bp}px)`)
 const mq = breakpoints.map(bp => `@media screen and (max-width: ${bp}px)`)
 const FooterEl = styled.footer`
-  margin: 0 auto 2em auto;
   display:flex;
   justify-content:flex-end;
   width:100%;
-  padding: 0 3em;
+  padding: 0 1em;
   ${mqHeight[0]} {
-    margin:16em auto 2em auto;
+    margin:0 auto 2em auto;
+  }
+  ${mq[0]} {
+    margin:0 auto 0 auto;
+    justify-content: center;
+    margin-bottom:3em;
+  }
+  ${mq[4]} {
+    
   }
 `
 const RulesButton = styled.button`
-  font-size: 1.4rem;
+  font-size: 2rem;
   background:transparent;
   color:white;
   padding:.45em 1.5em;
@@ -50,10 +57,12 @@ const RulesCloseButton = styled.span`
   font-family: 'Barlow Semi Condensed';
   color: #b5b8c8;
   cursor:pointer;
+  z-index:8;
   ${mq[1]} {
+    margin:0;
     top: initial;
-    right: 39%;
-    bottom: 4em;
+    right: 47%;
+    bottom: 1.3em;
   }
 `
 const RulesContainer = styled.div`
@@ -62,7 +71,7 @@ const RulesContainer = styled.div`
   background: white;
   height: 65vh;
   width: 75vw;
-  display: flex;
+  display:flex;
   justify-content: center;
   align-items: center;
   max-width:38.0rem;
@@ -77,7 +86,7 @@ const RulesContainer = styled.div`
 `
 const RulesTitle = styled.p`
   font-family: 'Barlow Semi Condensed';
-  margin:1em 0 0 .5em;
+  margin:1em 0 0 .9em;
   position:absolute;
   top:0;
   left:0;
@@ -85,9 +94,9 @@ const RulesTitle = styled.p`
   font-family: 'Fjalla One';
   font-weight: 800;
   ${mq[1]} {
-    left:initial;
+    left:4.2em;
     margin: 1em 0;
-    top:3em;
+    top:1.6em;
   }
 `
 const Footer = () => {
--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -2,38 +2,52 @@ import React, { useContext } from "react";
 import styled from "@emotion/styled";
 import { ScoreStateContext } from "./Layout";
 
-const breakpoints = [685]
+const breakpoints = [685, 414]
 
 const mq = breakpoints.map(bp => `@media (max-width: ${bp}px)`)
 
 const ScoreboardContainer = styled.section`
-  margin: 2em calc((100% - 30em)/3) 7em calc((100% - 30em)/3);
-  padding: 1.5em 1em;
+  margin: 5em calc((100% - 30em)/3.4) 7em calc((100% - 30em)/3.113);
+  padding: 1.6em 2.7em;
   border: 2px solid white;
-  border-radius: 20px;
+  border-radius: 5px;
   display:flex;
   align-items: center;
   justify-content: space-between;
-  line-height:1.5em;
-  letter-spacing: .02em;
+  line-height:2.33em;
+  letter-spacing: .2em;
   ${mq[0]} {
     margin: 2em calc((100% - 20em)/3) 7em calc((100% - 20em)/3);
+  }
+  ${mq[1]}{
+    margin:3em calc((100% - 20em)/5.7) 0em calc((100% - 20em)/5.7);
+    padding:1em;
+    line-height:.95em;
   }
   
 `
 const CopyContainer = styled.section`
   font-family: 'Fjalla One';
-  font-size: 1.6rem;
+  font-size: 2.6rem;
   font-weight: 800;
   color:white;
+  ${mq[1]}{
+    font-size: 1em;
+    margin-left:1.2em;
+  }
 `
 const ScoreContainer = styled.div`
   background-color:white;
-  height: 75px;
-  width: 110px;
+  height: 11.3em;
+  width: 15em;
   border-radius: 5px;
   position: relative;
   z-index:0;
+  padding:1em;
+  ${mq[1]}{
+    height: 7.4em;
+    width: 8em;
+  }
 `
 const ScoreCopyContainer = styled.div`
   position: relative;
@@ -46,16 +60,26 @@ const ScoreCopyContainer = styled.div`
   font-family: 'Barlow Semi Condensed';
   margin:.5em;
   line-height:initial;
+  height:100%;
+  ${mq[1]}{
+    margin:0 .5em;
+  }
 `
 const ScoreTitle = styled.p`
-  font-size: 1.4rem;
+  font-size: 1.8rem;
   letter-spacing: .075rem;
+  ${mq[1]}{
+    font-size: 1.3rem;
+  }
 `
 const ScoreNumber = styled.span`
   font-family: 'Fjalla One';
-  font-size: 4.5rem;
+  font-size: 5.5rem;
   line-height:auto;
   letter-spacing: auto;
+  ${mq[1]}{
+    font-size: 3.5rem;
+  }
 `
 const Scoreboard = () => {
   const score = useContext(ScoreStateContext)
