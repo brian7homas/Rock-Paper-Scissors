947d2275215ca67d7af6c88a37052bac9ca414c5
Brian Thomas <btdeveloper125@gmail.com>
2023-06-28 09:44:33 -0600

    feat(Board): MAIN BOARD ANIMATION
    reset animation
    iterate through hidden buttons to toggle position class
    Kill flip

--- src/animations/RestartAnimation.tsx
+++ src/animations/RestartAnimation.tsx
@@ -29,6 +29,43 @@ const RestartAnimation = (name: string, color: string) => {
   tl
     //? RESTART CONTAINER
     .fromTo('.restart-container', { display: 'flex', opacity: 1, visibility: 'visible', top: '0' }, { top: '4em', display: 'none', opacity: 0, visibility: 'hidden' }, '<')
+    //? LABELS
+    .fromTo(['.player-label', '.house-label'], { display: 'block', opacity: 1, visibility: 'visible' }, { display: 'none', opacity: 0, visibility: 'hidden' }, '0')
+    //? CONTAINER THAT HOLDS BOTH PLAYER ICONS
+    .to(`.icon-container`, { justifyContent: 'center', width: '0vw' }, '<')
+    //? OPPONENT
+    .to('.opponent-container', { transform: 'scale(1)', display: 'none', visibility: 'hidden', opacity: 0 }, '0')
+    .to('.opponent-inlay', { display: 'none', visibility: 'hidden', opacity: 0 }, '0')
+    //? RESET PLAYER CONTAINER BACKGROUND
+    .to(`.btn-container--${name}`, { duration: 1, background: `${color}`, zIndex: 2 }, '<')
+    //? ITERATE THROUGH HIDDEN BUTTONS AND BOARD PENTAGON
+    .fromTo([hidden, '.pentagon'],
+      {
+        opacity: 0, display: 'none', visibility: 'hidden', autoAlpha: 0
+      },
+      {
+        stagger: .2, autoAlpha: 1, visibility: 'visible', opacity: 1, display: 'flex', onComplete: () => {
+          gsap.to('.underlay', { display: 'none', visibility: 'hidden', opacity: 0 })
+        }
+      })
+
+    .to(`.board-container`, {
+      duration: .09,
+      justifyContent: 'center'
+    }, '0')
+    .then(() => {
+      hidden.map((el) => {
+        el.classList.toggle('hidden')
+      })
+      Flip.killFlipsOf([button,
+        '.opponent-container',
+        '.btn',
+        '.restart-container',
+        '.opponent-container',
+        '.opponent-inlay',
+        `.btn-container--${name}`])
+    }
+    )
 
 
   Flip.from(state, {
